e6ee89d068c73cd43015c98244602a2175062cf7
MohamedBoukthir <medboukthir7@gmail.com>
2026-05-22 14:24:25 +0100

    Refactor code structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,10 @@ import { DATA } from "@/data/resume";
 import Link from "next/link";
 import Markdown from "react-markdown";
 import ContactSection from "@/components/section/contact-section";
-import HackathonsSection from "@/components/section/hackathons-section";
 import ProjectsSection from "@/components/section/projects-section";
 import WorkSection from "@/components/section/experience-section";
 import { ArrowUpRight } from "lucide-react";
+import HackathonsSection from "@/components/section/hackathons-section";
 
 const BLUR_FADE_DELAY = 0.04;
 
